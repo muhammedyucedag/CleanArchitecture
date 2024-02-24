362feb4379eb68a3cb504a655eb0d7ff90d61303
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2024-02-24 14:28:21 +0300

    Presentation Katmanını WebApi Katmanına Bağlayalım

---
+++ CleanArchitecture.Presentation/AssemblyReference.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace CleanArchitecture.Presentation
+{
+    //Bu kod, Assembly sınıfının referansını oluşturarak, projede kullanılan Assembly özelliklerine erişimi sağlar.
+    public static class AssemblyReference
+    {
+        public static readonly Assembly Assembly = typeof(Assembly).Assembly;
+    }
+}
