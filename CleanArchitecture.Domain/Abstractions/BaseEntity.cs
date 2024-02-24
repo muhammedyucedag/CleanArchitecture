33f71d712158a992ccd6573c4ca31d0deb1efe79
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2024-02-24 15:08:44 +0300

    Database ve Entity Oluşturalım

---
+++ CleanArchitecture.Domain/Abstractions/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArchitecture.Domain.Abstractions;
+
+//abstract yaparak bir clasa bağımlı kıldık
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdateDate { get; set; }
+}
---
+++ CleanArchitecture.Domain/Entites/Car.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchitecture.Domain.Abstractions;
+
+namespace CleanArchitecture.Domain.Entites;
+
+//sealed olarak işaretlediğimiz için bu sınıftan başka bir sınıf oluşturulamaz veya miras alınamaz
+public sealed class Car : BaseEntity
+{
+    public string Name { get; set; }
+    public string Model { get; set; }
+    public int EnginePower { get; set; }
+}
