38290c4701efc9844a1268a69ca5afad90547c3d
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2024-02-24 16:36:07 +0300

    EntityTypeConfiguration, ModelCreating, SaveChangesAsync

--- CleanArchitecture.Domain/Abstractions/BaseEntity.cs
+++ CleanArchitecture.Domain/Abstractions/BaseEntity.cs
@@ -3,7 +3,12 @@
 //abstract yaparak bir clasa bağımlı kıldık
 public abstract class BaseEntity
 {
-    public Guid Id { get; set; }
+    public BaseEntity()
+    {
+        Id = Guid.NewGuid().ToString();
+    }
+
+    public string Id { get; set; }
     public DateTime CreatedDate { get; set; }
     public DateTime? UpdateDate { get; set; }
 }
--- CleanArchitecture.Persistance/Context/AppDbContext.cs
+++ CleanArchitecture.Persistance/Context/AppDbContext.cs
@@ -1,8 +1,29 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CleanArchitecture.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchitecture.Persistance.Context;
 
 public sealed class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssemblyReference).Assembly);
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entries = ChangeTracker.Entries<BaseEntity>();
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added)
+                entry.Property(p => p.CreatedDate)
+                    .CurrentValue = DateTime.Now; 
+
+            if (entry.State == EntityState.Modified)
+                entry.Property(p => p.UpdateDate)
+                    .CurrentValue = DateTime.Now;
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
 }
